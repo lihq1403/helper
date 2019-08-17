6241806b1c1f9f8c0123821c248f81194ef882a7
lihq-pc <lihqing@gmail.com>
2019-08-17 13:55:28 +0800

    定义常用时间常量

---
+++ src/Interfaces/DateGlobal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lihq1403\Interfaces;
+
+interface DateGlobal {
+    /**
+     * 一分钟的秒数
+     */
+    const SECONDS_IN_A_MINUTE = 60;
+
+    /**
+     * 一小时的秒数
+     */
+    const SECONDS_IN_A_HOUR = 3600;
+
+    /**
+     * 一天的秒数
+     */
+    const SECONDS_IN_A_DAY = 86400;
+
+    /**
+     * 一周的秒数
+     */
+    const SECONDS_IN_A_WEEK = 604800;
+}
